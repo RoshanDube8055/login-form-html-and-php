bc5c79ab72bfed8ebb9624b30a1c71b6b4c5400f
Roshan Dube <155941908+RoshanDube8055@users.noreply.github.com>
2024-03-10 05:03:49 -0600

    Add files via upload

---
+++ maxnumber.js
@@ -0,0 +1,10 @@
+const num1=parseInt("enter the frist number:");
+const num2=parseInt("enter the second number:");
+
+if(!isNaN(num) && !isNaN(num2)){
+    const sum=num1+num2;
+    console.log(sum);
+}
+else{
+    console.log("invalid statment");
+}
