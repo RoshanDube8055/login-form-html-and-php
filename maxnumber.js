04ae4d6272b9ad39ab40e6d4a166ac7f9e1195b3
Roshan Dube <155941908+RoshanDube8055@users.noreply.github.com>
2024-07-13 08:47:20 -0600

    Update maxnumber.js

--- maxnumber.js
+++ maxnumber.js
@@ -1,7 +1,7 @@
 const num1=parseInt("enter the frist number:");
 const num2=parseInt("enter the second number:");
 
-if(!isNaN(num) && !isNaN(num2)){
+if(!isNaN(num1) && !isNaN(num2)){
     const sum=num1+num2;
     console.log(sum);
 }
